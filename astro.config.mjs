d207f68fbf0af40413576e0da4e9c82be83e753e
tim <tfangmeyer@gmail.com>
2025-05-18 19:17:57 +0200

    Configure Astro for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://tifa365.github.io',
+  base: '/postautomaten_app',
   integrations: [],
   markdown: {
     shikiConfig: {
